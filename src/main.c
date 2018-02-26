18fc05c40bb84de2e64b5e61f3550e7b52e66207
Guillaume BRYON <gbryon@e2r11p22.42.fr>
2018-02-26 03:24:17 +0100

    ll

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 void	init(t_param *p)
 {
 	p->zoom = 0.005;
-	p->iter_max = 70;
+	p->iter_max = 40;
 	p->zoom_x = WH / 2;
 	p->zoom_y = HT / 2;
 	p->on = 1;
@@ -46,7 +46,7 @@ void	calculate_mandelbrot(t_param *p)
 		i++;
 	}
 	if (i == p->iter_max)
-		pixel_put(p, rgb((char)153, (char)51, (char)(204)));
+		pixel_put(p, rgb((char)153, (char)51, (char)204));
 	else
 		pixel_put(p, rgb((char)0, (char)51, (char)51));
 }
@@ -57,8 +57,6 @@ void	calculate_julia(t_param *p)
 
 	i = 0;
 	double	tmp;
-	p->c_r = 0.285;
-	p->c_i = 0.01;
 	p->z_r = (p->x - p->zoom_x) * p->zoom;
 	p->z_i = (p->y - p->zoom_y) * p->zoom;
 	while (p->z_r * p->z_r + p->z_i * p->z_i <= 4 && i < p->iter_max)
@@ -69,11 +67,50 @@ void	calculate_julia(t_param *p)
 		i++;
 	}
 	if (i == p->iter_max)
-		pixel_put(p, rgb((char)153, (char)51, (char)(204 / i) * i));
+		pixel_put(p, rgb((char)153, (char)51, (char)204));
 	else
 		pixel_put(p, rgb((char)0, (char)51, (char)51));
 }
 
+/*void	calculate_random(t_param *p)
+{
+	int		i;
+
+	i = 0;
+	p->z_r = 0.5 * (p->x - HT / 2) / (0.5 * p->zoom * HT);
+	p->z_i = (p->y - HT / 2) / (0.5 * p->zoom * HT);
+	double	tmp;
+	p->c_r = 0.3;
+	p->c_i = 0.9;
+	while (p->z_r * p->z_r + p->z_i * p->z_i <= 4 && i < p->iter_max)
+	{
+		tmp = p->z_r;
+		p->z_r = p->z_r * p->z_r - p->z_i * p->z_i + p->c_r;
+		p->z_i = 2 * p->z_i * tmp + p->c_i;
+		i++;
+	}
+	if (i == p->iter_max)
+		pixel_put(p, rgb((char)153, (char)51, (char)204));
+	else
+		pixel_put(p, rgb((char)0, (char)51, (char)51));
+}
+
+void	ft_random(t_param *p)
+{
+	p->y = 0;
+	while (p->y < HT)
+	{
+		p->x = 0;
+		while (p->x < WH)
+		{
+			calculate_random(p);
+			p->x++;
+		}
+		p->y++;
+	}
+	mlx_put_image_to_window(p->mlx, p->win, p->img, 0, 0);
+}
+*/
 void	julia(t_param *p)
 {
 	p->y = 0;
@@ -108,7 +145,10 @@ void	mandelbrot(t_param *p)
 
 void	usage(void)
 {
-	ft_putendl("usage: Mandelbrot: ./fractol [-m, -M, -mandelbrot]\n       Julia:      ./fractol [-j, -J, -julia]");
+	ft_putendl("usage: how to use: ./fractol [-info]");
+	ft_putendl("       Mandelbrot: ./fractol [-m, -M, -mandelbrot]");
+	ft_putendl("       Julia:      ./fractol [-j, -J, -julia]");
+	ft_putendl("       Bouddhabrot ./fractol [-b, -B, -bouddhabrot]");
 	exit(0);
 }
 
@@ -121,13 +161,31 @@ void	which_fractal(t_param *p)
 		p->fra = 1;
 	else if (ft_strcmp(p->argv, "-j") == 0 || ft_strcmp(p->argv, "-J") == 0
 			|| ft_strcmp(p->argv, "-julia") == 0)
+	{
 		p->fra = 2;
+		printf("julia");
+	}
+//	else if (ft_strcmp(p->argv, "-r") == 0 || ft_strcmp(p->argv, "-R") == 0
+//			|| ft_strcmp(p->argv, "-random") == 0)
+//		p->fra = 3;
+	else if (ft_strcmp(p->argv, "-info") == 0)
+	{
+		ft_putendl("\t\t__________________");
+		ft_putendl("\t\t|                |");
+		ft_putendl("\t\t|---USER GUIDE---|");
+		ft_putendl("\t\t|                |");
+		ft_putendl("\t\t|   ESC : quit   |");
+		ft_putendl("\t\t|ARROWS : MOVING |");
+		ft_putendl("\t\t|________________|");
+		exit (0);
+	}
 	else
 		usage();
 }
 
 int		keycode(int k, t_param *p)
 {
+	printf("k = %d\n", k);
 	if (k == ESC)
 		exit(0);
 	if (k == UP)
@@ -138,6 +196,21 @@ int		keycode(int k, t_param *p)
 		p->zoom_x -= 20;
 	if (k == RIGHT)
 		p->zoom_x += 20;
+	if (k == 69 && p->zoom < 99999999)
+		p->zoom -= 0.05 * p->zoom;
+	if (k == 78 && p->zoom > 0.05)
+		p->zoom += 0.05 * p->zoom;
+	if (k == 67)
+		p->c_r -= 0.002;
+
+	launcher(p);
+	return (0);
+}
+
+int		mouse_ev(int x, int y, t_param *p)
+{
+	p->c_r = (double)(x - WH / 2) / 150;
+	p->c_i = (double)(y - HT / 2) / 150;
 	launcher(p);
 	return (0);
 }
@@ -148,16 +221,18 @@ void	mlx_stuff(t_param *p)
 	p->win = mlx_new_window(p->mlx, WH, HT, "Fractol");
 	p->img = mlx_new_image(p->mlx, WH, HT);
 	mlx_hook(p->win, 2, 0, keycode, p);
+	mlx_hook(p->win, 6, 0, mouse_ev, p);
+
 }
-
-
 
 void	launcher(t_param *p)
 {
 	if (p->fra == 1)
 		mandelbrot(p);
-		if (p->fra == 2)
-			julia(p);
+	if (p->fra == 2)
+		julia(p);
+//	if (p->fra == 3)
+//		ft_random(p);
 }
 
 int		main(int ac, char **av)
